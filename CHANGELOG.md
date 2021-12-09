42667fda4a3a0436fddbf130f19883a6a9408120
Takehiro Shimokawa <shimokawa@mamezou.com>
2021-12-09 22:50:21 +0900

    UP2DATE

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,5 @@
-d716d2a (HEAD -> develop, origin/develop) UP2DATE
+390b222 (HEAD -> develop, origin/develop) UP2DATE
+d716d2a UP2DATE
 319bff3 UP2DATE
 d04c9ae UP2DATE
 df6476c UP2DATE
--- release.sh
+++ release.sh
@@ -10,7 +10,7 @@ fi
 tag=`date +%Y%m%d-%H%M%S`
 $GIT_PATH flow release start $tag
 $GIT_PATH log --oneline --decorate=short > CHANGELOG.md
-$GIT_PATH add .; git commit -m "UP2DATE"; git push
+$GIT_PATH add .; $GIT_PATH commit -m "UP2DATE"; $GIT_PATH push
 $GIT_PATH flow release finish $tag
 
 exit 0
